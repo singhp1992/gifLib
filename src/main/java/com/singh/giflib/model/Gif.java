d47e05b947235ced4e969a6147aa577586ba31bc
Preeti <singhp1992@gmail.com>
2019-07-25 11:21:32 +0200

    creating new gif model

---
+++ src/main/java/com/singh/giflib/model/Gif.java
@@ -0,0 +1,11 @@
+package com.singh.giflib.model;
+
+import java.time.LocalDate;
+
+public class Gif {
+    private String name;
+    private LocalDate dateUploaded;
+    private String username;
+    private boolean favorite;
+}
+
