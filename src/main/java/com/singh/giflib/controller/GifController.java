ed9529da8a00688c35611f71ae5925bdb24162ce
Preeti <singhp1992@gmail.com>
2019-07-25 11:48:55 +0200

    adding modelmap to gif details

--- src/main/java/com/singh/giflib/controller/GifController.java
+++ src/main/java/com/singh/giflib/controller/GifController.java
@@ -1,5 +1,6 @@
 package com.singh.giflib.controller;
 
+import com.singh.giflib.model.Gif;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,4 +11,9 @@ public class GifController {
         return "home";
     }
 
+    @RequestMapping("/gif")
+    public String gifDetails(ModelMap modelMap) {
+        Gif gif = new Gif("compiler-bot", LocalDate.of(2015,2,13), "Preeti Singh", true)
+        return "gif-details";
+    }
 }
