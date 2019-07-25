7c058cd88f1751d7f04419eceb7eb3d28a4c7f87
Preeti <singhp1992@gmail.com>
2019-07-25 12:04:40 +0200

    removed request mapping from another controller bc it was messing everything up

--- src/main/java/com/singh/giflib/controller/GifController.java
+++ src/main/java/com/singh/giflib/controller/GifController.java
@@ -2,6 +2,8 @@ package com.singh.giflib.controller;
 
 import com.singh.giflib.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import java.time.LocalDate;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,8 +14,9 @@ public class GifController {
     }
 
     @RequestMapping("/gif")
-    public String gifDetails(ModelMap modelMap) {
-        Gif gif = new Gif("compiler-bot", LocalDate.of(2015,2,13), "Preeti Singh", true)
+    public String gifDetails(Model model) {
+        Gif gif = new Gif("compiler-bot", LocalDate.of(2015,2,13), "Preeti Singh", true);
+        model.addAttribute("gif", gif);
         return "gif-details";
     }
 }
