172cdc8e24d97a0ea3df01500fa9a9f00a000ec4
Preeti <singhp1992@gmail.com>
2019-07-25 13:24:32 +0200

    added new gifs to array

--- src/main/java/com/singh/giflib/data/GifRepository.java
+++ src/main/java/com/singh/giflib/data/GifRepository.java
@@ -1,4 +1,15 @@
 package com.singh.giflib.data;
 
+import com.singh.giflib.model.Gif;
+import java.util.List;
+
 public class GifRepository {
+    private static final List<Gif> ALL_GIFS = Arrays.asList(
+            new Gif("android-explosion", LocalDate.of(2015,2,13), "Chris Ramacciotti", false),
+            new Gif("ben-and-mike", LocalDate.of(2015,10,30), "Ben Jakuben", true),
+            new Gif("book-dominos", LocalDate.of(2015,9,15), "Craig Dennis", false),
+            new Gif("compiler-bot", LocalDate.of(2015,2,13), "Ada Lovelace", true),
+            new Gif("cowboy-coder", LocalDate.of(2015,2,13), "Grace Hopper", false),
+            new Gif("infinite-andrew", LocalDate.of(2015,8,23), "Marissa Mayer", true)
+    );
 }
