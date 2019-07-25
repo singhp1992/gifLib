88883a23d69e398b8fa6b84b0a9b62e43c59d70e
Preeti <singhp1992@gmail.com>
2019-07-25 16:54:48 +0200

    path working

--- src/main/java/com/singh/giflib/controller/GifController.java
+++ src/main/java/com/singh/giflib/controller/GifController.java
@@ -18,9 +18,10 @@ public class GifController {
         return "home";
     }
 
+//     http://localhost:8080/gif/android-explosion
     @RequestMapping("/gif/{name}")
     public String gifDetails(@PathVariable String name, Model model) {
-        Gif gif = gifRepository.findByName("android-explosion");
+        Gif gif = gifRepository.findByName(name);
         model.addAttribute("gif", gif);
         return "gif-details";
     }
