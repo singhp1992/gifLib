79636f9d42e4cd922db67dc998ab1c2a56b8d356
Preeti <singhp1992@gmail.com>
2019-07-29 09:21:41 +0200

    students working

--- src/main/java/com/singh/giflib/controller/StudentController.java
+++ src/main/java/com/singh/giflib/controller/StudentController.java
@@ -1,12 +1,11 @@
 package com.singh.giflib.controller;
 
-import com.singh.giflib.data.GifRepository;
+import com.singh.giflib.data.PersonRepository;
 import com.singh.giflib.data.StudentRepository;
 import com.singh.giflib.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -17,7 +16,7 @@ public class StudentController {
     private StudentRepository studentRepository;
 
     @Autowired
-    private GifRepository gifRepository;
+    private PersonRepository gifRepository;
 
     @RequestMapping("/students")
     public String listCategories(Model model) {
@@ -26,14 +25,4 @@ public class StudentController {
         return "students";
     }
 
-    @RequestMapping("/students/{id}")
-    public String student(@PathVariable int id, Model model) {
-        Student student = studentRepository.findById(id);
-        model.addAttribute("student", student);
-
-//        List<Student> students = gifRepository.findByStudentId(id);
-//        model.addAttribute("students", students);
-
-        return "category";
-    }
 }
